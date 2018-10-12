705bc2a0490f1c52632ede3bcca58606e3632ccd
root1 <Parazitxxx@mail.ru>
2018-10-12 09:52:11 -0700

    add fabric in class

--- test_visitor.py
+++ test_visitor.py
@@ -1,6 +1,8 @@
 import pytest
 import visitor
-visitor = visitor.Visitor()
+
+my_visitor = visitor.Visitor.create_visitor(123456, "Vasiliy",
+                                            "parazit@gmail.com")
 
 
 def test_return_visitor():
@@ -10,12 +12,12 @@ def test_return_visitor():
     """
 
     # Create test visitor
-    visitor.create_visitor(123456, "Vasiliy", "parazit@gmail.com")
-    name = visitor.get_name()
-    email = visitor.get_email()
-    visitor_id = visitor.get_id()
-    visitor.add_book_id(1234, 5678, 9101112, 7889)
-    used_book_id = visitor.get_used_books_id()
+
+    name = my_visitor.name
+    email = my_visitor.email
+    visitor_id = my_visitor.visitor_id
+    my_visitor.add_in_backpack(1234, 5678, 9101112, 7889)
+    used_book_id = my_visitor.used_books_id
 
     # Check return data
     assert name == "Vasiliy"
@@ -26,71 +28,69 @@ def test_return_visitor():
 # If delivered empty.
 
 
-def test_passes_id():
+def test_empty_id():
     with pytest.raises(Exception):
-        visitor.create_visitor(" ", "dsds", "parazit@gmail.com")
+        visitor.Visitor.create_visitor(" ", "dsds", "parazit@gmail.com")
 
 
-def test_passes_name():
+def test_empty_name():
     with pytest.raises(Exception):
-        visitor.create_visitor(123456, " ", "parazit@gmail.com")
+        visitor.Visitor.create_visitor(123456, " ", "parazit@gmail.com")
 
 
-def test_passes_email():
+def test_empty_email():
     with pytest.raises(Exception):
-        visitor.create_visitor(123456, "dsds", " ")
+        visitor.Visitor.create_visitor(123456, "dsds", " ")
 
 
-def test_passes_user_book_id():
+def test_empty_add_in_backpack():
     with pytest.raises(Exception):
-        visitor.add_book_id(" ")
+        my_visitor.add_in_backpack(" ")
 
 
 # If delivered short.
 
-def test_passes_id_short():
+def test_id_short():
     with pytest.raises(Exception):
-        visitor.create_visitor(123, "Vasiliy", "parazit@gmail.com")
+        visitor.Visitor.create_visitor(123, "Vasiliy", "parazit@gmail.com")
 
 
-def test_passes_name_short():
+def test_name_short():
     with pytest.raises(Exception):
-        visitor.create_visitor(123456, "Vas", "parazit@gmail.com")
+        visitor.Visitor.create_visitor(123456, "Vas", "parazit@gmail.com")
 
 
-def test_passes_email_short():
+def test_email_short():
     with pytest.raises(Exception):
-        visitor.create_visitor(123456, "Vasiliy", "par")
+        visitor.Visitor.create_visitor(123456, "Vasiliy", "p@u.r")
 
 
-def test_passes_user_book_id_short():
+def test_add_in_backpack_short():
     with pytest.raises(Exception):
-        visitor.add_book_id(12)
+        my_visitor.add_in_backpack(844)
 
 # If delivered long.
 
 
-def test_passes_id_long():
+def test_id_long():
     with pytest.raises(Exception):
-        visitor.create_visitor(123478988567891, "Vasiliy", "parazit@gmail.com")
+        visitor.Visitor.create_visitor(123478988567891, "Vasiliy",
+                                       "parazit@gmail.com")
 
 
-def test_passes_name_long():
+def test_name_long():
     with pytest.raises(Exception):
-        visitor.create_visitor(123456, "Vasiluiiygrtdefrt", "parazit@gmail.com")
+        visitor.Visitor.create_visitor(123456, "Vasiluiiygrtdefrt",
+                                       "parazit@gmail.com")
 
 
-def test_passes_email_long():
+def test_email_long():
     with pytest.raises(Exception):
-        visitor.create_visitor(123456, "Vasiliy",
+        visitor.Visitor.create_visitor(123456, "Vasiliy",
                          "parazitxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
                          "xxxxxxxxxxxxxxxxxxxxx@gmail.com")
 
 
-def test_passes_user_book_id_long():
+def test_add_in_backpack_long():
     with pytest.raises(Exception):
-        visitor.add_book_id(123457877798789797978779778976)
-
-
-
-
+        my_visitor.add_in_backpack(844345345345345345345354354345345)
--- visitor.py
+++ visitor.py
@@ -6,81 +6,87 @@ class Visitor:
 
     Class show human which come in library.
     For create visitor use: create_visitor()
-    Add and delete books in class visitor: add_book_id(), delete_used_book_id
-    Get data about visitor, use: get_name/ id/ email/ used_book_id()
+    Add and del books in visitor backpack: add_in_backpack(), del_of_backpack
+    Get data about visitor, use fields:used_books_id, visitor_id,
+    name, email
+
     """
 
     # Mask for email
     reg = '(^|\s)[-a-z0-9_.]+@([-a-z0-9]+\.)+[a-z]{2,6}(\s|$)'
 
-    def __init__(self):
+    # restriction length data
+    _min_length_data = 3
+    _max_length_data = 15
+    _max_length_email = 64
+
+    def __init__(self, visitor_id, name, email):
         """ this list saved which books used visitors"""
-        self._used_books_id = []
+        self.used_books_id = []
 
-        # restriction length data
-        self._min_length_data = 3
-        self._max_length_data = 15
-        self._max_length_email = 64
+        self.visitor_id = visitor_id
+        self.name = name
+        self.email = email
 
-    def create_visitor(self, visitor_id, name, email):
+    @classmethod
+    def create_visitor(cls, visitor_id, name, email):
         """Create visitor in library and test his data."""
 
-        if isinstance(name, str) and name != "" and self._min_length_data <\
-                len(str(name)) < self._max_length_data:
-            self._name = name
-        else:
-            raise ValueError("Delivered empty name or not expected \
-                                quantity symbols in name or they is digit")
+        arguments = []
 
         if isinstance(visitor_id, int) and visitor_id != "" and \
-                self._min_length_data < len(str(abs(visitor_id))) < \
-                self._max_length_data:
-            self._visitor_id = visitor_id
+                cls._min_length_data < len(str(abs(visitor_id))) < \
+                cls._max_length_data:
+            if cls.check_id(visitor_id):
+                arguments.append(visitor_id)
+            else:
+                raise ValueError("Use unique ID!")
+
         else:
-            raise ValueError("Delivered empty name or not expected \
-                                quantity symbols in name or they is digit")
+            raise ValueError("Delivered empty name or not expected "
+                             "quantity symbols in name or they is digit")
+
+        if isinstance(name, str) and name != "" and cls._min_length_data <\
+                len(str(name)) < cls._max_length_data:
+            arguments.append(name)
+        else:
+            raise ValueError("Delivered empty name or not expected "
+                             "quantity symbols in name or they is digit")
 
         # EMAIL
 
         email = email.lower()
-        if email != "" and self._min_length_data < len(str(email)) <\
-                self._max_length_email and re.fullmatch(self.reg, email):
-            self._email = email
+        if email != "" and cls._min_length_data < len(str(email)) <\
+                cls._max_length_email and re.fullmatch(cls.reg, email):
+            arguments.append(email)
         else:
             raise Exception("!Delivered empty  Email or bad Email address")
 
+        return cls(*arguments)
+
         # Check and saved this which books used visitors
 
-    def add_book_id(self, *used_books_id):
-        """Adding ID new used book for visitor"""
+    # this function check id
+    @classmethod
+    def check_id(cls, user_id):
+        cls.check_id = user_id
+        raise NotImplemented("Error !")
+    #   return True
+
+    # if ID unique then return TRUE else return FALSE
+
+    def add_in_backpack(self, *used_books_id):
+        """Adding ID new used book for backpack visitor"""
 
         for book_id in used_books_id:
             if isinstance(book_id, int) and book_id != "" and \
                     self._min_length_data < len(str(book_id)) <\
                     self._max_length_data:
-                self._used_books_id.append(book_id)
+                self.used_books_id.append(book_id)
             else:
-                raise ValueError("Delivered empty usedBookID or not\
-                                    expected quantity symbols in usedBookID")
+                raise ValueError("Delivered empty usedBookID or not "
+                                 "expected quantity symbols in usedBookID")
 
-    def delete_used_book_id(self, del_book_id):
+    def del_of_backpack(self, del_book_id):
         """Deleting ID used book for visitor"""
-        self._used_books_id.remove(del_book_id)
-
-    """Next four metods return data about visitor"""
-    def get_name(self):
-        return self._name
-
-    def get_id(self):
-        return self._visitor_id
-
-    def get_email(self):
-        return self._email
-
-    def get_used_books_id(self):
-        return self._used_books_id
-
-
-
-
-
+        self.used_books_id.remove(del_book_id)
