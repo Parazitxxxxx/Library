a2275014cfe659e980d6813d4dd6f000ab1891a2
root1 <Parazitxxx@mail.ru>
2018-11-03 02:58:57 -0700

    refrash book and visitor

--- test_visitor.py
+++ test_visitor.py
@@ -1,96 +1,33 @@
 import pytest
 import visitor
+import book
 
-my_visitor = visitor.Visitor.create_visitor(123456, "Vasiliy",
-                                            "parazit@gmail.com")
+my_visitor = visitor.Visitor(123456, "Vasiliy", "Ivanovich",
+                             "parazit@gmail.com")
+my_book = book.Book('Turgenev', 1978, '978-5-389-01985-0')
+my_book2 = book.Book('Turgenev2', 1222, '978-5-389-01985-2')
 
 
 def test_return_visitor():
-    """Check return data. If I call create_visitor(1234, "vasil' .....)
+    """Check return data. If I call create Visitor(1234, "vasil' .....)
 
-    Then class return expected by me data.
+    Then instance return expected by me data.
     """
 
     # Create test visitor
 
-    name = my_visitor.name
+    first_name = my_visitor.first_name
+    last_name = my_visitor.last_name
     email = my_visitor.email
     visitor_id = my_visitor.visitor_id
-    my_visitor.add_in_backpack(1234, 5678, 9101112, 7889)
-    used_book_id = my_visitor.used_books_id
+    """Add book in backpack"""
+    my_visitor.add_in_backpack(my_book)
+    my_visitor.add_in_backpack(my_book2)
+    used_book = my_visitor.used_books
 
     # Check return data
-    assert name == "Vasiliy"
+    assert first_name == "Vasiliy"
+    assert last_name == "Ivanovich"
     assert email == "parazit@gmail.com"
     assert visitor_id == 123456
-    assert used_book_id == [1234, 5678, 9101112, 7889]
-
-# If delivered empty.
-
-
-def test_empty_id():
-    with pytest.raises(Exception):
-        visitor.Visitor.create_visitor(" ", "dsds", "parazit@gmail.com")
-
-
-def test_empty_name():
-    with pytest.raises(Exception):
-        visitor.Visitor.create_visitor(123456, " ", "parazit@gmail.com")
-
-
-def test_empty_email():
-    with pytest.raises(Exception):
-        visitor.Visitor.create_visitor(123456, "dsds", " ")
-
-
-def test_empty_add_in_backpack():
-    with pytest.raises(Exception):
-        my_visitor.add_in_backpack(" ")
-
-
-# If delivered short.
-
-def test_id_short():
-    with pytest.raises(Exception):
-        visitor.Visitor.create_visitor(123, "Vasiliy", "parazit@gmail.com")
-
-
-def test_name_short():
-    with pytest.raises(Exception):
-        visitor.Visitor.create_visitor(123456, "Vas", "parazit@gmail.com")
-
-
-def test_email_short():
-    with pytest.raises(Exception):
-        visitor.Visitor.create_visitor(123456, "Vasiliy", "p@u.r")
-
-
-def test_add_in_backpack_short():
-    with pytest.raises(Exception):
-        my_visitor.add_in_backpack(844)
-
-# If delivered long.
-
-
-def test_id_long():
-    with pytest.raises(Exception):
-        visitor.Visitor.create_visitor(123478988567891, "Vasiliy",
-                                       "parazit@gmail.com")
-
-
-def test_name_long():
-    with pytest.raises(Exception):
-        visitor.Visitor.create_visitor(123456, "Vasiluiiygrtdefrt",
-                                       "parazit@gmail.com")
-
-
-def test_email_long():
-    with pytest.raises(Exception):
-        visitor.Visitor.create_visitor(123456, "Vasiliy",
-                         "parazitxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-                         "xxxxxxxxxxxxxxxxxxxxx@gmail.com")
-
-
-def test_add_in_backpack_long():
-    with pytest.raises(Exception):
-        my_visitor.add_in_backpack(844345345345345345345354354345345)
+    assert used_book == [my_book, my_book2]
--- visitor.py
+++ visitor.py
@@ -1,92 +1,31 @@
-import re
+import book as Book
 
 
 class Visitor:
-    """Class made for library.
+    """Class made for visitor in library.
 
     Class show human which come in library.
-    For create visitor use: create_visitor()
+    For create visitor use: Visitor(id, first_name, last_name, email)
     Add and del books in visitor backpack: add_in_backpack(), del_of_backpack
-    Get data about visitor, use fields:used_books_id, visitor_id,
-    name, email
-
+    Get data about visitor, use fields: visitor_id, first_name, last_name,
+    email.
+    For get data about all books in backpack use fields: used_books, which
+    return list: (instance Book)
     """
-
-    # Mask for email
-    reg = '(^|\s)[-a-z0-9_.]+@([-a-z0-9]+\.)+[a-z]{2,6}(\s|$)'
-
-    # restriction length data
-    _min_length_data = 3
-    _max_length_data = 15
-    _max_length_email = 64
-
-    def __init__(self, visitor_id, name, email):
-        """ this list saved which books used visitors"""
-        self.used_books_id = []
+    def __init__(self, visitor_id: int, first_name: str,
+                 last_name: str, email: str):
 
         self.visitor_id = visitor_id
-        self.name = name
+        self.first_name = first_name
+        self.last_name = last_name
         self.email = email
+        """ this list saved which books used visitors"""
+        self.used_books = []
 
-    @classmethod
-    def create_visitor(cls, visitor_id, name, email):
-        """Create visitor in library and test his data."""
+    def add_in_backpack(self, used_book:Book):
+            """Adding instance new used book for backpack visitor"""
+            self.used_books.append(used_book)
 
-        arguments = []
-
-        if isinstance(visitor_id, int) and visitor_id != "" and \
-                cls._min_length_data < len(str(abs(visitor_id))) < \
-                cls._max_length_data:
-            if cls.check_id(visitor_id):
-                arguments.append(visitor_id)
-            else:
-                raise ValueError("Use unique ID!")
-
-        else:
-            raise ValueError("Delivered empty name or not expected "
-                             "quantity symbols in name or they is digit")
-
-        if isinstance(name, str) and name != "" and cls._min_length_data <\
-                len(str(name)) < cls._max_length_data:
-            arguments.append(name)
-        else:
-            raise ValueError("Delivered empty name or not expected "
-                             "quantity symbols in name or they is digit")
-
-        # EMAIL
-
-        email = email.lower()
-        if email != "" and cls._min_length_data < len(str(email)) <\
-                cls._max_length_email and re.fullmatch(cls.reg, email):
-            arguments.append(email)
-        else:
-            raise Exception("!Delivered empty  Email or bad Email address")
-
-        return cls(*arguments)
-
-        # Check and saved this which books used visitors
-
-    # this function check id
-    @classmethod
-    def check_id(cls, user_id):
-        cls.check_id = user_id
-        raise NotImplemented("Error !")
-    #   return True
-
-    # if ID unique then return TRUE else return FALSE
-
-    def add_in_backpack(self, *used_books_id):
-        """Adding ID new used book for backpack visitor"""
-
-        for book_id in used_books_id:
-            if isinstance(book_id, int) and book_id != "" and \
-                    self._min_length_data < len(str(book_id)) <\
-                    self._max_length_data:
-                self.used_books_id.append(book_id)
-            else:
-                raise ValueError("Delivered empty usedBookID or not "
-                                 "expected quantity symbols in usedBookID")
-
-    def del_of_backpack(self, del_book_id):
-        """Deleting ID used book for visitor"""
-        self.used_books_id.remove(del_book_id)
+    def del_of_backpack(self, del_book):
+        """Deleting instance used book for visitor"""
+        self.used_books.remove(del_book)
